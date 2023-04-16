218cb6f783f35d72d390a6cc6be271b4cc711b52
rfnkurle <45407665+rfnkurle@users.noreply.github.com>
2023-04-16 14:02:27 -0600

    customer table

--- schema.sql
+++ schema.sql
@@ -2,8 +2,17 @@ CREATE DATABASE movie_booking;
 
 USE movie_booking;
 
+-- create films table
 CREATE TABLE films(
 id INT PRIMARY KEY AUTO_INCREMENT,
 name VARCHAR(30) NOT NULL UNIQUE,
 length_min INT NOT NULL
+);
+
+-- customer table
+CREATE TABLE customers (
+id INT PRIMARY KEY AUTO_INCREMENT,
+first_name VARCHAR(30),
+last_name VARCHAR(30) NOT NULL,
+email VARCHAR(30)NOT NULL
 );
